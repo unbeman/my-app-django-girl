bb05be81907231d6a02dbd9f5ea94ead9a168de4
unbeman <unbeman@gmail.com>
2016-01-10 23:29:08 +0300

    close

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth.views import login, logout
+from django.contrib.auth.decorators import login_required
+
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'', include('blog.urls')),
+    url(r'', include('blog.urls', namespace='blog')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/', login, {'template_name':'blog/login.html'}, name='login'),
+    url(r'^logout/', logout, {'template_name':'blog/post_list.html'}, name='logout'),
 ]
